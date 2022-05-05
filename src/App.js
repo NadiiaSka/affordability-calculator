a8c32bfdc147f05e6f603d5bee1f4cf101245fc6
NadiiaSka <syvakivska@gmail.com>
2022-05-05 18:47:38 +1200

    Implement buttons row

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import ButtonRow from "./components/ButtonRow";
+
 function App() {
   return (
     <div className="Container">
       <h1>Calculator</h1>
+      <p>How many of you are buying the property?</p>
+      <ButtonRow />
     </div>
   );
 }
---
+++ src/components/ButtonRow.js
@@ -0,0 +1,10 @@
+const ButtonRow = () => {
+  return (
+    <>
+      <button className="btn">Just me</button>
+      <button className="btn">I'm buying with someone</button>
+    </>
+  );
+};
+
+export default ButtonRow;
