504188c1730d1a6bfb27f14283e3f22f559e4405
NadiiaSka <syvakivska@gmail.com>
2022-05-08 21:29:23 +1200

    improve unit tests for the button component

--- src/tests/button.test.js
+++ src/tests/button.test.js
@@ -1,28 +1,23 @@
 import { render, fireEvent, screen } from "@testing-library/react";
 import Button from "../components/Button";
 
+const handleButtonSelectedMock = jest.fn();
+
+beforeEach(() => {
+  render(
+    <Button
+      label="test button"
+      className="btn"
+      handleButtonSelected={handleButtonSelectedMock}
+    />
+  );
+});
+
 describe("Button Component", () => {
   test("renders the button correctly", () => {
-    const handleButtonSelectedMock = jest.fn();
-    render(
-      <Button
-        label="test button"
-        className="btn-selected"
-        handleButtonSelected={handleButtonSelectedMock}
-      />
-    );
-    const button = screen.getByText("test button");
-    expect(button).toBeInTheDocument();
+    expect(screen.getByText("test button")).toBeInTheDocument();
   });
   test("calls the handleButtonClick function when clicked", () => {
-    const handleButtonSelectedMock = jest.fn();
-    render(
-      <Button
-        label="test button"
-        className="btn-selected"
-        handleButtonSelected={handleButtonSelectedMock}
-      />
-    );
     const button = screen.getByText("test button");
     fireEvent.click(button);
     expect(handleButtonSelectedMock).toHaveBeenCalledTimes(1);
